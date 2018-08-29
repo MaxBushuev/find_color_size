d89892ff7d5de39713cf1e30f86597618d2c1b90
MaxBushuev <36811783+MaxBushuev@users.noreply.github.com>
2018-08-30 01:11:51 +0300

    Update colorset.cpp

--- colorset.cpp
+++ colorset.cpp
@@ -18,11 +18,11 @@ int main()
 	int lengthmin=0;
 	int lengthmax=0;
 
-	int sootmin = 50;
-	int sootmax = 50;
+	int ratiomin = 50;
+	int ratiomax = 50;
 
-	float sootmind;
-	float sootmaxd;
+	float ratiomind;
+	float ratiomaxd;
 	VideoCapture cap(0);
 	
 	while (true)
@@ -30,19 +30,19 @@ int main()
 
 		cap >> img;
 		cvtColor(img, hsv, COLOR_RGB2HSV);
-		createTrackbar("Hmin", "Red", &findhred, 180);
-		createTrackbar("Smin", "Red", &findsred, 255);
-		createTrackbar("Vmin", "Red", &findvred, 255);
-		createTrackbar("Hmax", "Red", &findhblue, 180);
-		createTrackbar("Smax", "Red", &findsblue, 255);
-		createTrackbar("Vmax", "Red", &findvblue, 255);
+		createTrackbar("Hmin", "Color", &findhred, 180);
+		createTrackbar("Smin", "Color", &findsred, 255);
+		createTrackbar("Vmin", "Color", &findvred, 255);
+		createTrackbar("Hmax", "Color", &findhblue, 180);
+		createTrackbar("Smax", "Color", &findsblue, 255);
+		createTrackbar("Vmax", "Color", &findvblue, 255);
 
 		createTrackbar("Lengthmin", "Result", &lengthmin, 1000);
 		createTrackbar("sootmin", "Result", &sootmin, 180);
 		createTrackbar("Lenghtmax", "Result", &lengthmax, 1000);
 		createTrackbar("sootmax", "Result", &sootmax, 180);
-		sootmind = sootmin / 100;
-		sootmaxd = sootmax / 100;
+		ratiomind = ratiomin / 100;
+		ratiomaxd = ratiomax / 100;
 		inRange(hsv, Scalar(findhred, findsred, findvred), Scalar(findhblue, findsblue, findvblue), poiskred);
 		imshow("Red", poiskred);
 		vector<vector<Point>> contours;
@@ -60,11 +60,11 @@ int main()
 		{
 			if (boundRect[i].height > lengthmin && boundRect[i].width > lengthmin&& boundRect[i].height < lengthmax && boundRect[i].width < lengthmax && sootmind < boundRect[i].height / boundRect[i].width < sootmaxd)
 			{
-				drawContours(poiskred, contours_poly, (int)i, Scalar(0, 255, 0), 1, 8, vector<Vec4i>(), 0, Point());
+				drawContours(findRed, contours_poly, (int)i, Scalar(0, 255, 0), 1, 8, vector<Vec4i>(), 0, Point());
 				rectangle(img, boundRect[i].tl(), boundRect[i].br(), Scalar(0, 255, 0), 2, 8, 0);
 			}
 		}
-		imshow("Result", poiskred);
+		imshow("Result", findRed);
 		imshow("Final", img);
 		waitKey(1);
 	}
